332694ebc18c26b08a7b03983f9ddf0703695238
nguy3n <nguyen.chu@hotmail.com>
2017-04-26 16:01:07 +0200

    App has 2 activities in which the string is passed when you click on the button in the first activity.

---
+++ Android/GameOfLifeAndroid/app/src/main/java/no/hioa/gameoflifeandroid/GameActivity.java
@@ -0,0 +1,23 @@
+package no.hioa.gameoflifeandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+
+    }
+
+
+}
