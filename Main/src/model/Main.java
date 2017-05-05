01040e222628973871b6c8e906ccff1ae635b422
Andre <andre_fagereng@hotmail.com>
2017-05-05 23:32:07 +0200

    Smaller changes

--- Main/src/model/Main.java
+++ Main/src/model/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/mainGUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainGUI.fxml"));
         primaryStage.setTitle("Game Of Life");
         Scene scene = new Scene(root);
         scene.getStylesheets().add("Style.css");
